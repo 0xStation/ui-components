2ae723b03eba81e9173e0fc708109ebfa52aa0e6
gelicamarie <turla_angelica@yahoo.com>
2023-07-07 09:53:11 +0100

    update package.json

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   name: 'components',
-  entry: ['src/**/*.{ts,tsx}'],
+  entry: ['src', '!src/**/*.stories.{ts,tsx}', '!src/**/*.spec.{ts,tsx}', '!src/stories'],
   splitting: true,
   sourcemap: true,
   clean: true,
