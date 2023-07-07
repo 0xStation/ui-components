8cfd89cfa3e6e38cb64a3e9ce700628edd0f17c2
gelicamarie <turla_angelica@yahoo.com>
2023-07-07 23:46:11 +0100

    prettier

--- packages/components/src/icons/action/ChevronUp.tsx
+++ packages/components/src/icons/action/ChevronUp.tsx
@@ -14,12 +14,11 @@ export const ChevronUp = ({
       className={clsx(className || 'w-6 h-6 aspect-square')}
       {...props}
     >
-            <path
+      <path
         fillRule="evenodd"
         d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z"
         clipRule="evenodd"
       />
     </svg>
-
   )
 }
