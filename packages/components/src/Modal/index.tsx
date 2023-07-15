87d157750f698b918397a0aa6179756ef7f4b461
gelicamarie <turla_angelica@yahoo.com>
2023-07-15 13:40:27 +0100

    modal updates

--- packages/components/src/Modal/index.tsx
+++ packages/components/src/Modal/index.tsx
@@ -15,7 +15,9 @@ export function Modal({
     <Root {...props}>
       <Portal>
         <Overlay className="ui-fixed ui-inset-0 ui-z-40 ui-bg-[rbga(0,0,0,0.5)] ui-backdrop-blur-sm  data-[state=open]:ui-animate-in data-[state=closed]:ui-animate-out data-[state=closed]:ui-fade-out-0 data-[state=open]:ui-fade-in-0" />
-        <ModalContext.Provider value={{ size: props.size }}>{children}</ModalContext.Provider>
+        <ModalContext.Provider value={{ size: props.size, closeModal: props.closeModal }}>
+          {children}
+        </ModalContext.Provider>
       </Portal>
     </Root>
   )
@@ -24,23 +26,30 @@ export function Modal({
 export const ModalContent = React.forwardRef<
   React.ElementRef<typeof Content>,
   React.ComponentPropsWithoutRef<typeof Content> & { hideClose?: boolean }
->(({ className, hideClose = false, children, ...props }, ref) => (
-  <Content
-    ref={ref}
-    className={cn(
-      'ui-fixed ui-left-[50%] ui-top-[50%] ui-z-50 ui-grid ui-w-full ui-max-w-xl ui-translate-x-[-50%] ui-translate-y-[-50%] ui-border ui-bg-black ui-pt-10 ui-pb-4 ui-px-5 ui-shadow-lg ui-duration-200 data-[state=open]:ui-animate-in data-[state=closed]:ui-animate-out data-[state=closed]:ui-fade-out-0 data-[state=open]:ui-fade-in-0 data-[state=closed]:ui-zoom-out-95 data-[state=open]:ui-zoom-in-95 data-[state=closed]:ui-slide-out-to-left-1/2 data-[state=closed]:ui-slide-out-to-top-[48%] data-[state=open]:ui-slide-in-from-left-1/2 data-[state=open]:ui-slide-in-from-top-[48%] ui-rounded-md md:ui-w-full ui-text-white ui-border-wet-concrete100 ui-gap-4',
-      className,
-    )}
-    {...props}
-  >
-    {!hideClose && (
-      <Close className="ui-absolute ui-right-4 ui-top-4 [&_svg]:ui-text-violet20 focus-visible:ui-outline-wet-concrete100 group">
-        <CloseIcon className="ui-w-6 ui-h-6 ui-fill-concrete100 group-hover:ui-fill-white ui-transition-all" />
-      </Close>
-    )}
-    {children}
-  </Content>
-))
+>(({ className, hideClose = false, children, ...props }, ref) => {
+  const { closeModal } = useModalContext()
+  return (
+    <Content
+      ref={ref}
+      className={cn(
+        'ui-fixed ui-left-[50%] ui-top-[50%] ui-z-50 ui-grid ui-w-full ui-max-w-xl ui-translate-x-[-50%] ui-translate-y-[-50%] ui-border ui-bg-black ui-pt-10 ui-pb-4 ui-px-5 ui-shadow-lg ui-duration-200 data-[state=open]:ui-animate-in data-[state=closed]:ui-animate-out data-[state=closed]:ui-fade-out-0 data-[state=open]:ui-fade-in-0 data-[state=closed]:ui-zoom-out-95 data-[state=open]:ui-zoom-in-95 data-[state=closed]:ui-slide-out-to-left-1/2 data-[state=closed]:ui-slide-out-to-top-[48%] data-[state=open]:ui-slide-in-from-left-1/2 data-[state=open]:ui-slide-in-from-top-[48%] ui-rounded-md md:ui-w-full ui-text-white ui-border-wet-concrete100 ui-gap-4',
+        className,
+      )}
+      {...props}
+    >
+      {!hideClose && !!closeModal && (
+        <Close
+          asChild
+          onClick={closeModal}
+          className="ui-absolute ui-right-4 ui-top-4 [&_svg]:ui-text-violet20 focus-visible:ui-outline-wet-concrete100 group"
+        >
+          <CloseIcon className="ui-w-6 ui-h-6 ui-fill-concrete100 group-hover:ui-fill-white ui-transition-all" />
+        </Close>
+      )}
+      {children}
+    </Content>
+  )
+})
 
 const headingVariants = cva('ui-font-favoritpro ui-text-marbleWhite ui-capitalize', {
   variants: {
